486366b2c6e803ef1b967360192533b92cbabd9c
Benyi Wang <bwang@napster.com>
2017-09-08 19:17:29 +0000

    move DriveFactory from ChartComputeDriver to Driver.Factory add Driver.run(sparkSession => Unit) to hide creating Driver factory.

--- src/main/scala/com/napster/bi/sparkapp/Driver.scala
+++ src/main/scala/com/napster/bi/sparkapp/Driver.scala
@@ -1,9 +1,30 @@
 package com.napster.bi.sparkapp
 
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.apache.spark.sql.SparkSession
 
 trait Driver extends LazyLogging {
 
   def run(): Unit
+
+}
+
+object Driver {
+
+  type Factory = SparkSession => Driver
+
+  /**
+    * Return a factory which creates a Driver running the specified function
+    *
+    * @param f the driver's run function.
+    * @return the factory create the driver.
+    */
+  def run(f: SparkSession => Unit): Factory = { spark: SparkSession =>
+
+    new Driver {
+      override def run(): Unit = f(spark)
+    }
+
+  }
 
 }
